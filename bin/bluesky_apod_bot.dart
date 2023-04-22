6fd5ab3f34efa45d4fb5be8bbb65981aef965b32
myConsciousness <contact@shinyakato.dev>
2023-04-22 16:37:23 +0900

    update

--- bin/bluesky_apod_bot.dart
+++ bin/bluesky_apod_bot.dart
@@ -10,7 +10,7 @@ import 'package:nasa/nasa.dart';
 bsky.Record? repostRecord;
 
 void main(List<String> args) async {
-  Cron().schedule(Schedule.parse('0 0 10,22 * *'), () async {
+  Cron().schedule(Schedule.parse('0 10,22 * * *'), () async {
     final bluesky = bsky.Bluesky.fromSession(
       await _session,
       retryConfig: bsky.RetryConfig(
