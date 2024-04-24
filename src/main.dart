67a61ce5121025c96e809440edf16c3edc0622e9
Shinya Kato <contact@shinyakato.dev>
2024-04-24 18:21:56 +0900

    use aws_lambda_dart_runtime_ns

--- src/main.dart
+++ src/main.dart
@@ -1,6 +1,24 @@
-import 'aws/runtime.dart';
-import 'function.dart' as fn;
+import 'package:aws_lambda_dart_runtime_ns/aws_lambda_dart_runtime_ns.dart';
+
+import 'post/post_today.dart';
 
 void main() {
-  handler('main.today', fn.today);
+  AwsLambdaRuntime()
+    ..addHandler(
+      FunctionHandler(
+        name: 'main.today',
+        action: (context, event) async {
+          await postToday();
+
+          return InvocationResult(
+            requestId: context.requestId,
+            body: {
+              'statusCode': 200,
+              'body': event,
+            },
+          );
+        },
+      ),
+    )
+    ..invoke();
 }
