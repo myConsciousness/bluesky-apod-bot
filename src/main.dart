012f18fc09826484678c93e3eec1e68af8f03f7e
Shinya Kato <contact@shinyakato.dev>
2024-04-27 10:49:55 +0900

    bump

--- src/main.dart
+++ src/main.dart
@@ -1,23 +1,7 @@
 import 'package:aws_lambda_dart_runtime_ns/aws_lambda_dart_runtime_ns.dart';
 
-import 'post/post_today.dart';
+import 'functions.dart' as fn;
 
-Future<void> main() async {
-  await AwsLambdaRuntime()
-      .addHandler(
-        FunctionHandler(
-          name: 'main.today',
-          action: (context, event) async {
-            await postToday();
-
-            return InvocationResult(
-              requestId: context.requestId,
-              body: {
-                'message': 'success',
-              },
-            );
-          },
-        ),
-      )
-      .invoke();
-}
+Future<void> main() async => await invokeAwsLambdaRuntime([
+      fn.postToday,
+    ]);
